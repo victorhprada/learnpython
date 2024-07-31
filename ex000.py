a491a77b60e3b000ea7ecdbf6a8c660e057fb502
Victor Hugo P Teixeira <54296181+victorhprada@users.noreply.github.com>
2024-07-30 21:22:47 -0300

    Add files via upload

---
+++ ex000.py
@@ -0,0 +1,5 @@
+primeiro = int(input('Digite o primeiro número'))
+segundo = int(input('Digite o segundo número'))
+resultado = primeiro + segundo
+# print('O primeiro é ', primeiro, '. O segundo é ', segundo, '. E o resultado é',resultado)
+print(f'A some entre o primeiro {primeiro} e o segundo {segundo} é {resultado}')
---
+++ ex002.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite a nota da primeira prova: '))
+n2 = int(input('Digite a nota da segunda nota: '))
+m = (n1 + n2)/2
+print(f'A primeira nota digitada é {n1} a segunda nota digitada é {n2} e a média entre as notas é {m}')
