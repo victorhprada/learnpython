41f8ddb37c669a28b0968317b30ac0d4a93fc739
victo <victorhugopteixeira@hotmail.com>
2024-08-05 21:39:14 -0300

    Mais um commit 2

--- ex010.py
+++ ex010.py
@@ -5,6 +5,6 @@ d = qd * 60
 kmt = km * 0.15
 t = d + kmt
 
-print(f'As diárias foram de {qd} dias e o valor foi de {d}')
-print(f'Os km rodados foram de {km} Km e o valor total foi de R$ {kmt}')
+print(f'As diárias foram de {qd} dias e o valor foi de {d}');
+print(f'Os km rodados foram de {km} Km e o valor total foi de R$ {kmt}');
 print(f'O total é R$ {t:.2f}');
